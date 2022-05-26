b645fb4c67699e2c299a4150ac43c1d155e09ad3
Muhammad Al Faruqi <muhammad.faruqi@btpn.com>
2022-05-26 09:53:11 +0700

    chore: initiate commit

---
+++ src/main/java/pygmy/spring/boilerplate/SpringBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package pygmy.spring.boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pygmy/spring/boilerplate/SpringBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package pygmy.spring.boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
